4ad1d7d9bf3e928ee3748c077f7e3a28ac66f5b4
Kyleed68 <kyleedvodohi@gmail.com>
2025-04-10 22:35:28 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "enter your session",Caseyrhodes~IOdl2Khb#UYCKbyHXMNqX4Tf03zr4GshVQAPTTN2YYbMTz8dokXc
     CAPTION: process.env.CAPTION || "*ᴘᴏᴡᴅᴇʀᴇᴅ ʙʏ ᴛᴀʟᴋᴅʀᴏᴠᴇ*",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
